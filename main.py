1ab837225b779bddb99732740206d4917ca7690a
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2024-05-20 10:22:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,12 @@ class Board:
         self.board = board
 
     def __str__(self):
-        upper_lines = f'\n╔═══{"╤═══"*2}{"╦═══"}{"╤═══"*2}{"╦═══"}{"╤═══"*2}╗\n'
-        middle_lines = f'╟───{"┼───"*2}{"╫───"}{"┼───"*2}{"╫───"}{"┼───"*2}╢\n'
-        lower_lines = f'╚═══{"╧═══"*2}{"╩═══"}{"╧═══"*2}{"╩═══"}{"╧═══"*2}╝\n'
-        board_string = upper_lines
-        for index, line in enumerate(self.board):
-            row_list = []
-            for square_no, part in enumerate([line[:3], line[3:6], line[6:]], start=1):
-                row_square = '|'.join(str(item) for item in part)
-                row_list.extend(row_square)
-                if square_no != 3:
-                    row_list.append('║')
-
-            row = f'║ {" ".join(row_list)} ║\n'
-            row_empty = row.replace('0', ' ')
-            board_string += row_empty
-
-            if index < 8:
-                if index % 3 == 2:
-                    board_string += f'╠═══{"╪═══"*2}{"╬═══"}{"╪═══"*2}{"╬═══"}{"╪═══"*2}╣\n'
-                else:
-                    board_string += middle_lines
-            else:
-                board_string += lower_lines
-
-        return board_string
+        board_str = ''
+        for row in self.board:
+            row_str = [str(i) if i else '*' for i in row]
+            board_str += ' '.join(row_str)
+            board_str += '\n'
+        return board_str
 
     def find_empty_cell(self):
         for row, contents in enumerate(self.board):
@@ -42,14 +23,11 @@ class Board:
         return num not in self.board[row]
 
     def valid_in_col(self, col, num):
-        return all(
-            self.board[row][col] != num
-            for row in range(9)
-        )
+        return all(self.board[row][col] != num for row in range(9))
 
     def valid_in_square(self, row, col, num):
         row_start = (row // 3) * 3
-        col_start=(col // 3) * 3
+        col_start = (col // 3) * 3
         for row_no in range(row_start, row_start + 3):
             for col_no in range(col_start, col_start + 3):
                 if self.board[row_no][col_no] == num:
@@ -66,27 +44,24 @@ class Board:
     def solver(self):
         if (next_empty := self.find_empty_cell()) is None:
             return True
-        else:
-            for guess in range(1, 10):
-                if self.is_valid(next_empty, guess):
-                    row, col = next_empty
-                    self.board[row][col] = guess
-                    if self.solver():
-                        return True
-                    self.board[row][col] = 0
-
+        for guess in range(1, 10):
+            if self.is_valid(next_empty, guess):
+                row, col = next_empty
+                self.board[row][col] = guess
+                if self.solver():
+                    return True
+                self.board[row][col] = 0
         return False
 
 def solve_sudoku(board):
     gameboard = Board(board)
-    print(f'\nPuzzle to solve:\n{gameboard}')
+    print(f'Puzzle to solve:\n{gameboard}')
     if gameboard.solver():
-        print('\nSolved puzzle:')
-        print(gameboard)
-
+        print(f'Solved puzzle:\n{gameboard}')
     else:
-        print('\nThe provided puzzle is unsolvable.')
+        print('The provided puzzle is unsolvable.')
     return gameboard
+
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
   [0, 0, 0, 0, 0, 3, 7, 6, 2],
